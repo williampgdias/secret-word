d19d766388563176bca9a6e6277a678660884425
William Dias <smdwilliam89@gmail.com>
2023-01-20 23:36:06 +0000

    Criando o projeto

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  car: ['Engine', 'Door', 'Hood', 'Wheels', 'Antenna'],
+  fruit: ['Banana', 'Apple', 'Pear', 'Papaya', 'Orange'],
+  body: ['Arm', 'Leg', 'Brain', 'Neck', 'Eyes'],
+  pc: ['Mouse', 'Keyboard', 'Monitor', 'Cabinet'],
+  programmer: ['Language', 'Framework', 'JavaScript', 'React'],
+  food: ['Rice', 'Beans', 'Meat', 'Milk', 'Egg'],
+};
