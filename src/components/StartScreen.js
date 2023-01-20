9dc75e9e26ea7be013935b5eb125a130663679b0
William Dias <smdwilliam89@gmail.com>
2023-01-20 23:49:34 +0000

    Finalizando tela inicial

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css';
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h1>Secret Word</h1>
+      <p>Click the button below to start playing</p>
+      <button>Start the game</button>
+    </div>
+  );
+};
+
+export default StartScreen;
