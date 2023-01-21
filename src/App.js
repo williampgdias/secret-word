311f2ccbae82d3e05b66fb2251ba6d41614924af
William Dias <smdwilliam89@gmail.com>
2023-01-21 00:21:23 +0000

    Criando componentes do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+// CSS
 import './App.css';
+
+// REACT
+import { useCallback, useEffect, useState } from 'react';
+
+// DATA
+import { wordsList } from './data/words';
+
+// COMPONENTS
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import './Game.css';
+
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div>Game</div>
+  )
+}
+
+export default Game
