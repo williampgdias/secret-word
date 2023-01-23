8a5b0eceedbdfb7f4bd7b555849611508035530f
William Dias <smdwilliam89@gmail.com>
2023-01-23 23:00:06 +0000

    Estruturando componente do jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,9 +4,28 @@ import React from 'react';
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>End the game</button>
+    <div className='game'>
+      <p className='score'>Score: 000</p>
+      <h1>Guess the word:</h1>
+      <h3 className='tip'>
+        Tip about the word: <span>Tip...</span>
+      </h3>
+      <div className='wordContainer'>
+        <span className='letter'>A</span>
+        <span className='blankSquare'></span>
+      </div>
+      <div className='letterContainer'>
+        <p>Try to guess a letter word:</p>
+        <form>
+          <input type='text' name='letter' maxLength='1' required />
+          <button>Play!</button>
+        </form>
+      </div>
+      <div className='wrongLetterContainer'>
+        <p>Letter already used</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   );
 };
