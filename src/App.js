c57150e91b3d0291e1bccde32c9ecbd3b4234d50
William Dias <smdwilliam89@gmail.com>
2023-03-06 14:41:13 +0000

    Finalizando tela de fim de jogo

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
